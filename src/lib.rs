4bfd2abebf43023ef86250fa62e023c2a0f7c15b
swwind <i@sww.moe>
2024-04-14 19:23:21 +0800

    make it more fast (maybe?)

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use std::borrow::Borrow;
+use std::rc::Rc;
 
-use swc_common::input::StringInput;
-use swc_common::BytePos;
-use swc_ecmascript::ast::{Callee, Decl, EsVersion, ExportDecl, Expr, Pat};
+use swc_common::comments::SingleThreadedComments;
+use swc_common::input::SourceFileInput;
+use swc_common::{FileName, SourceMap};
+use swc_ecmascript::ast::{Callee, Decl, EsVersion, Expr, Module, ModuleDecl, ModuleItem, Pat};
 use swc_ecmascript::parser::lexer::Lexer;
 use swc_ecmascript::parser::{Parser, Syntax, TsConfig};
-use swc_ecmascript::visit::{Visit, VisitWith};
 use wasm_bindgen::prelude::*;
 
 pub struct Found {
@@ -24,23 +25,27 @@ impl Found {
   }
 }
 
-impl Visit for FindExportsVisitor {
-  fn visit_export_decl(&mut self, n: &ExportDecl) {
-    if let Decl::Var(var_decl) = &n.decl {
-      if let Some(decl) = var_decl.decls.first() {
-        if let Pat::Ident(ident_export) = &decl.name {
-          if let Some(init) = &decl.init {
-            if let Expr::Call(call) = init.borrow() {
-              if let Callee::Expr(expr) = &call.callee {
-                if let Expr::Ident(ident_callee) = expr.borrow() {
-                  let callee_name = ident_callee.sym.to_string();
-                  let export_name = ident_export.sym.to_string();
+impl FindExportsVisitor {
+  fn find_module(&mut self, module: &Module) {
+    for item in &module.body {
+      if let ModuleItem::ModuleDecl(ModuleDecl::ExportDecl(decl)) = item {
+        if let Decl::Var(var_decl) = &decl.decl {
+          if let Some(decl) = var_decl.decls.first() {
+            if let Pat::Ident(ident_export) = &decl.name {
+              if let Some(init) = &decl.init {
+                if let Expr::Call(call) = init.borrow() {
+                  if let Callee::Expr(expr) = &call.callee {
+                    if let Expr::Ident(ident_callee) = expr.borrow() {
+                      let callee_name = ident_callee.sym.to_string();
+                      let export_name = ident_export.sym.to_string();
 
-                  if self.matches.contains(&callee_name) {
-                    self.exports.push(Found {
-                      callee: callee_name,
-                      export: export_name,
-                    })
+                      if self.matches.contains(&callee_name) {
+                        self.exports.push(Found {
+                          callee: callee_name,
+                          export: export_name,
+                        })
+                      }
+                    }
                   }
                 }
               }
@@ -54,14 +59,18 @@ impl Visit for FindExportsVisitor {
 
 #[wasm_bindgen]
 pub fn find_exports(source: &str, callees: Vec<String>) -> Vec<String> {
+  let cm = Rc::new(SourceMap::default());
+  let fm = cm.new_source_file(FileName::Custom("input.js".to_string()), source.to_string());
+
+  let comments = SingleThreadedComments::default();
   let lexer = Lexer::new(
     Syntax::Typescript(TsConfig {
       tsx: true,
       ..Default::default()
     }),
-    EsVersion::Es2022,
-    StringInput::new(source, BytePos(0), BytePos(source.as_bytes().len() as u32)),
-    None,
+    EsVersion::EsNext,
+    SourceFileInput::from(&*fm),
+    Some(&comments),
   );
 
   let mut parser = Parser::new_from(lexer);
@@ -71,7 +80,86 @@ pub fn find_exports(source: &str, callees: Vec<String>) -> Vec<String> {
     matches: callees,
     exports: Vec::new(),
   };
-  module.visit_with(&mut visitor);
+  visitor.find_module(&module);
 
   visitor.exports.into_iter().map(|x| x.to_string()).collect()
+}
+
+#[cfg(test)]
+mod test {
+  use crate::find_exports;
+
+  fn run_test(source: &str, callees: &[&str], expected: &[&str]) {
+    let result = find_exports(source, callees.iter().map(|x| x.to_string()).collect());
+    assert_eq!(result, expected);
+  }
+
+  #[test]
+  fn support_pure_javascript() {
+    run_test(
+      r#"
+      export const one = loader$(() => { return true; });
+      export let two = action$(() => { return false; });
+      export var the = loader$(() => { return false; });
+    
+      // below may works, but will not match
+      const none = loader$(() => {}); export { none };
+      export const none = (0, loader$)(() => {});
+      export const [none = loader$(() => {})] = [];
+      "#,
+      &["loader$", "action$"],
+      &["loader$/one", "action$/two", "loader$/the"],
+    );
+  }
+
+  #[test]
+  fn support_typescript() {
+    run_test(
+      r#"
+      export const one: Loader = loader$<T>(() => { return true; });
+      export const two: Action = action$<{ name: string }>(() => { return false; });
+      export const the: {} | null = loader$(() => { return false; });
+      "#,
+      &["loader$", "action$"],
+      &["loader$/one", "action$/two", "loader$/the"],
+    );
+  }
+
+  #[test]
+  fn support_jsx() {
+    run_test(
+      r#"
+      export const one: Loader = loader$<div>(() => { return <div> hello world </div>; });
+      export const two: Action = action$(() => { return <span> export const v = loader$() </span>; });
+      export const the: {} | null = loader$(() => { return null; });
+      "#,
+      &["loader$", "action$"],
+      &["loader$/one", "action$/two", "loader$/the"],
+    );
+  }
+
+  #[test]
+  fn support_esnext() {
+    run_test(
+      r#"
+      class A { static #foo = 233 };
+      class B { static { console.log("block"); } }
+      await getMethod(undefined ?? "233");
+      "#,
+      &["loader$", "action$"],
+      &[],
+    );
+  }
+
+  #[test]
+  #[should_panic]
+  fn invalid_code() {
+    run_test(
+      r#"
+      a7268t612t*^T@&^(!%&^T@^!!R&@^(TR^!@T(*R!TR%)!@&%^(*@&!%(@!)()()())
+      "#,
+      &["loader$", "action$"],
+      &[],
+    );
+  }
 }
